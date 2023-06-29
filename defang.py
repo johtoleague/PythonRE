525e55761e02cfae6db018e88327fb9ee19e5941
johtoleague <jon.doanne@gmail.com>
2023-06-29 07:50:59 -0600

    s

---
+++ defang.py
@@ -0,0 +1,15 @@
+def defang(ip : str) -> str:
+    # return ip.replace(".", "[.]")
+
+    ans = ''
+    for ch in ip:
+        if ch == ".":
+            ans += "[.]"
+        else:
+            ans += ch
+    return ans
+
+
+testip = "192.168.22.22"
+
+print(defang(testip))
