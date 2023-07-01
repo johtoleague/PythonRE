e0a7125a643eb4b179bd992a8d62d118b01d40a5
johtoleague <jon.doanne@gmail.com>
2023-07-01 09:02:02 -0600

    as

--- merge.py
+++ merge.py
@@ -17,3 +17,12 @@ def merge(nums1:list[int], m: int, nums2: list[int], n: int) -> None:
         nums1[p] = nums2[p2]
         p2 -= 1
         p -= 1
+        
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+merge(nums1, m, nums2, n)
+print(nums1) 
+
+
